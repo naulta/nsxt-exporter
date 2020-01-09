1d1d4fd5ebb12bb45e14629e61662aa94627d6d8
naulta <anault@ecsteam.com>
2020-01-09 06:47:55 -0700

    fixed node json structs

--- nsxt_cluster_nodes_collector.go
+++ nsxt_cluster_nodes_collector.go
@@ -48,116 +48,116 @@ type ClusterInitNodeInfo struct {
 
 //ClusterNodeStatus Definitions
 type ClusterNodeStatus struct {
-	nodeSystemStatus      NodeStatusProperties  `json:"system_status"`
-	nodeMgmtClusterStatus NodeMgmtClusterStatus `json:"mgmt_cluster_status"`
-	nodeVersion           string                `json:"version"`
+	NodeSystemStatus      NodeStatusProperties  `json:"system_status"`
+	NodeMgmtClusterStatus NodeMgmtClusterStatus `json:"mgmt_cluster_status"`
+	NodeVersion           string                `json:"version"`
 }
 
 type NodeStatusProperties struct {
-	memUsed         int32            `json:"mem_used"`
-	sysTime         int32            `json:"system_time"`
-	nodeFileStatus  []NodeFileStatus `json:"file_systems"`
-	nodeLoadAverage []float64        `json:"load_averages"`
-	nodeSwapTotal   int32            `json:"swap_total"`
-	nodeMemCache    int32            `json:"mem_cache"`
-	nodeCPUs        int32            `json:"cpu_cores"`
-	nodeSource      string           `json:"source"`
-	nodeMemTotal    int32            `json:"mem_total"`
-	nodeSwapUsed    int32            `json:"swap_used"`
-	nodeUpTime      int32            `json:"uptime"`
+	MemUsed         int32            `json:"mem_used"`
+	SysTime         int32            `json:"system_time"`
+	NodeFileStatus  []NodeFileStatus `json:"file_systems"`
+	NodeLoadAverage []float64        `json:"load_averages"`
+	NodeSwapTotal   int32            `json:"swap_total"`
+	NodeMemCache    int32            `json:"mem_cache"`
+	NodeCPUs        int32            `json:"cpu_cores"`
+	NodeSource      string           `json:"source"`
+	NodeMemTotal    int32            `json:"mem_total"`
+	NodeSwapUsed    int32            `json:"swap_used"`
+	NodeUpTime      int32            `json:"uptime"`
 }
 
 type NodeFileStatus struct {
-	fileSystemName string `json:"file_system"`
-	diskTotal      int32  `json:"total"`
-	diskUsed       int32  `json:"used"`
-	fileSystemType string `json:"type"`
-	mountName      string `json:"mount"`
+	FileSystemName string `json:"file_system"`
+	DiskTotal      int32  `json:"total"`
+	DiskUsed       int32  `json:"used"`
+	FileSystemType string `json:"type"`
+	MountName      string `json:"mount"`
 }
 
 type NodeMgmtClusterStatus struct {
-	nodeMgmtClusterStatus string `json:"mgmt_cluster_status"`
+	MgmtClusterStatus string `json:"mgmt_cluster_status"`
 }
 
 // EdgeClusterResponse and Node Definitions
 type EdgeClustersResponse struct {
-	cursor       string         `json:"cursor"`
-	resultCount  int16          `json:"result_count"`
-	edgeClusters []EdgeClusters `json:"results"`
+	Cursor       string         `json:"cursor"`
+	ResultCount  int16          `json:"result_count"`
+	EdgeClusters []EdgeClusters `json:"results"`
 }
 
 type EdgeClusters struct {
-	revision               string                 `json:"_revision"`
-	id                     string                 `json:"is"`
-	resourceType           string                 `json:"resource_type"`
-	clusterProfileBindings ClusterProfileBindings `json:"cluster_profile_bindings"`
-	edgeNoodes             []EdgeNodes            `json:"members"`
-	systemOwned            string                 `json:"_system_owned"`
-	deploymentType         string                 `json:"deployment_type"`
-	lastModifiedUser       string                 `json:"_last_modified_user"`
-	lastModifiedTime       int32                  `json:"_last_modified_time"`
-	createTime             int32                  `json:"_create_time`
-	createUser             string                 `json:"_create_user`
+	Revision               string                 `json:"_revision"`
+	Id                     string                 `json:"is"`
+	ResourceType           string                 `json:"resource_type"`
+	ClusterProfileBindings ClusterProfileBindings `json:"cluster_profile_bindings"`
+	EdgeNoodes             []EdgeNodes            `json:"members"`
+	SystemOwned            string                 `json:"_system_owned"`
+	DeploymentType         string                 `json:"deployment_type"`
+	LastModifiedUser       string                 `json:"_last_modified_user"`
+	LastModifiedTime       int32                  `json:"_last_modified_time"`
+	CreateTime             int32                  `json:"_create_time`
+	CreateUser             string                 `json:"_create_user`
 }
 
 type ClusterProfileBindings struct {
-	resourceType string `json:"resource_type"`
-	profileID    string `json:"profile_id"`
+	ResourceType string `json:"resource_type"`
+	ProfileID    string `json:"profile_id"`
 }
 
 type EdgeNodes struct {
-	id    string `json:"transport_node_id"`
-	index string `json:"member_index"`
+	Id    string `json:"transport_node_id"`
+	Index string `json:"member_index"`
 }
 
 type EdgeClusterStatus struct {
-	id             string           `json:"edge_cluster_id"`
-	edgeNodeStatus []EdgeNodeStatus `json:"member_status"`
-	lastUpdateTime int32            `json:"last_update_timestamp"`
-	status         string           `json:"edge_cluster_status"`
+	Id             string           `json:"edge_cluster_id"`
+	EdgeNodeStatus []EdgeNodeStatus `json:"member_status"`
+	LastUpdateTime int32            `json:"last_update_timestamp"`
+	Status         string           `json:"edge_cluster_status"`
 }
 
 type EdgeNodeStatus struct {
-	id     string `json:"transport_node_id"`
-	status string `json:"status"`
+	Id     string `json:"transport_node_id"`
+	Status string `json:"status"`
 }
 
 // IP Pools and Blocks Definitions
 type IPPoolsResponse struct {
-	cursor        string    `json:"cursor"`
-	sortBy        string    `json:"sort_by"`
-	sortAscending bool      `json:"sort_ascending"`
+	Cursor        string    `json:"cursor"`
+	SortBy        string    `json:"sort_by"`
+	SortAscending bool      `json:"sort_ascending"`
 	IPPools       []IPPools `json:"results"`
 }
 
 type IPPools struct {
-	revision         string    `json:"_revision"`
-	id               string    `json:"id"`
-	name             string    `json:"display_name"`
-	description      string    `json:"description"`
-	resourceType     string    `json:"description"`
-	subnets          []subnets `json:"description"`
-	lastModifiedUser string    `json:"_last_modified_user"`
-	lastModifiedTime int32     `json:"_last_modified_time"`
-	createTime       int32     `json:"_create_time`
-	createUser       string    `json:"_create_user`
+	Revision         string    `json:"_revision"`
+	Id               string    `json:"id"`
+	Name             string    `json:"display_name"`
+	Description      string    `json:"description"`
+	ResourceType     string    `json:"description"`
+	Subnets          []subnets `json:"description"`
+	LastModifiedUser string    `json:"_last_modified_user"`
+	LastModifiedTime int32     `json:"_last_modified_time"`
+	CreateTime       int32     `json:"_create_time`
+	CreateUser       string    `json:"_create_user`
 }
 
 type subnets struct {
-	dnsNameservers   []string           `json:"dns_nameservers"`
-	allocationRanges []AllocationRanges `json:"allocation_ranges"`
-	gatewayIP        string             `json:"gateway_ip"`
-	cidr             string             `json:"cidr"`
+	DnsNameservers   []string           `json:"dns_nameservers"`
+	AllocationRanges []AllocationRanges `json:"allocation_ranges"`
+	GatewayIP        string             `json:"gateway_ip"`
+	Cidr             string             `json:"cidr"`
 }
 
 type AllocationRanges struct {
-	startIP string `json:"start"`
-	endIP   string `json:"end"`
+	StartIP string `json:"start"`
+	EndIP   string `json:"end"`
 }
 
 type IPPoolAllocations struct {
-	count       int32    `json:"result_count"`
-	allocatedIP []string `json:"allocation_id"`
+	Count       int32    `json:"result_count"`
+	AllocatedIP []string `json:"allocation_id"`
 }
 
 //Define the metrics we wish to expose
@@ -362,8 +362,8 @@ func getNsxClusterNodeMetrics(id string, clusterid string) {
 	var responseObject ClusterNodeStatus
 	json.Unmarshal(responseData, &responseObject)
 
-	if (debug) { log.Println("Controller cluster node status:" + responseObject.nodeMgmtClusterStatus.nodeMgmtClusterStatus)}
-	mgmtClusterNodeStatusMetric.WithLabelValues(clusterid, id, responseObject.nodeMgmtClusterStatus.nodeMgmtClusterStatus).Set(1.0)
+	if (debug) { log.Println("Controller cluster node status:" + responseObject.NodeMgmtClusterStatus.MgmtClusterStatus)}
+	mgmtClusterNodeStatusMetric.WithLabelValues(clusterid, id, responseObject.NodeMgmtClusterStatus.MgmtClusterStatus).Set(1.0)
 
 	diff := time.Now().Sub(t1)
 	fmt.Println(diff)
